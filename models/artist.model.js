8ec7a8f1f9ebeb2a652632951d9a6737185faf86
TEGUIA KAMEGNE ABEL DIMITRI <135368563+Teguia-Abel-Pycoder@users.noreply.github.com>
2025-01-18 21:59:30 +0100

    Update artist.model.js

--- models/artist.model.js
+++ models/artist.model.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/db');  // Sequelize 
+const sequelize = require('../config/db');  //  
 
 const Artist = sequelize.define('Artist', {
   artistId: {
