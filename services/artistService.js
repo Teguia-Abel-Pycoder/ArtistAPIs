d98c6ed2eec818bf324a29a63145df6436b1bae0
TEGUIA KAMEGNE ABEL DIMITRI <135368563+Teguia-Abel-Pycoder@users.noreply.github.com>
2025-01-21 23:37:46 +0100

    Update artistService.js

--- services/artistService.js
+++ services/artistService.js
@@ -19,7 +19,7 @@ const getArtistsLogic = async () => {
   }
 };
 
-// Delete 
+// Delete
 const deleteArtistsLogic = async (ids) => {
   try {
     const artistIds = ids.split(',').map(id => parseInt(id));
