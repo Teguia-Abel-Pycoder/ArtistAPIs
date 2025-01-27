5b0701d11e41449c48a20e9e29531cc00c04543b
TEGUIA KAMEGNE ABEL DIMITRI <135368563+Teguia-Abel-Pycoder@users.noreply.github.com>
2025-01-27 09:41:27 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@
 //   database: process.env.DB_NAME || 'artistdb',
 //   waitForConnections: true,
 //   connectionLimit: 10,
-//   queueLi
+//   queueL
 // // Export the po
 const { Sequelize } = require('sequelize');
 
