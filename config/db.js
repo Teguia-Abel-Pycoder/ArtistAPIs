cd01c236b3a76e3827ff3b198bd80dafcb4e671f
TEGUIA KAMEGNE ABEL DIMITRI <135368563+Teguia-Abel-Pycoder@users.noreply.github.com>
2025-01-30 16:51:00 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@
 // const pool = mysql.createPool({
 //   host: process.env.DB_HOST || 'localhost',
 //   user: process.env.DB_USER || '
-r
+
 //   password: process.env.DB_PASSWORD || '1830',
 //   database: process.env.DB_NAME || 'artistdb',
 //   waitForConnections: true
