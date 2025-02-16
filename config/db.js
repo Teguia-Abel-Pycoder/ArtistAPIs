02428637e9f58a76ae9cb273ce02cafebadbe23b
TEGUIA KAMEGNE ABEL DIMITRI <135368563+Teguia-Abel-Pycoder@users.noreply.github.com>
2025-02-16 13:29:58 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 tthh// const mysql = require('mysql2');
 // require('dotenv').config(); // To use environmen variables
 // // Create a MySQL 
-// cons
+// con
 //   password: process.env.DB_ || 183'
 //   database: process.env.ggDB_NAME || 'artistdb',
 const { Sequelize } = require('sequelize');
