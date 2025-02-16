8bbb4f48dece3e86f283ac790c5ff75426080a09
TEGUIA KAMEGNE ABEL DIMITRI <135368563+Teguia-Abel-Pycoder@users.noreply.github.com>
2025-02-16 13:31:18 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ tthh// const mysql = require('mysql2');
 // // Create a MySQL
 //
 //   password: process.env.DB_ || 18
-//   database: process.env.ggDB_NAME || 'artistdb',
+//   database: process.env.ggDB_NAME || artistdb',
 const { Sequelize } = require('sequelize');
 // Set up the connection to the MySQL 
 const sequelize = new Sequelize('sql5755850', 'sql5755850', 'yA6Bby83hg', {
